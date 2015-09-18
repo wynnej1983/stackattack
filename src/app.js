7e608cd5ed42d4b77b397c4d05055500404be777
Jonathan Wynne <wynnej1983@gmail.com>
2015-09-18 15:14:03 +0100

    remove old res files

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ var HelloWorldLayer = cc.Layer.extend({
         // 3. add your codes below...
         // add a label shows "Hello World"
         // create and initialize a label
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
+        var helloLabel = new cc.LabelTTF("Hello StackAttack", "Arial", 38);
         // position the label on the center of the screen
         helloLabel.x = size.width / 2;
         helloLabel.y = size.height / 2 + 200;
